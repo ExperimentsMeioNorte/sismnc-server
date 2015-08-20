f6eafd616876aacf35f3761d046890e7e3c3ad58
Alisson <alissonplus@msn.com>
2015-08-20 00:35:56 -0300

    Zion
    - criado o metodo routers

--- admin/lib/System.js
+++ admin/lib/System.js
@@ -34,50 +34,6 @@ Meteor.zion = {
         }
     },
 
-    television: {
-        controller: RouteController.extend({
-            layoutTemplate: 'television',
-            yieldTemplates: {
-                'header': {to: 'headerTelevision'},
-            }
-        })
-    },
-
-    newspaper: {
-        controller: RouteController.extend({
-            layoutTemplate: 'newspaper',
-            yieldTemplates: {
-                'header': {to: 'headerNewspaper'},
-            }
-        })
-    },
-
-    portal: {
-        controller: RouteController.extend({
-            layoutTemplate: 'portal',
-            yieldTemplates: {
-                'header': {to: 'headerPortal'},
-            }
-        })
-    },
-
-    radio: {
-        controller: {
-          boafm: RouteController.extend({
-              layoutTemplate: 'fmmn',
-              yieldTemplates: {
-                  'header': {to: 'headerRadioFMMN'},
-              }
-          }),
-          fmmn: RouteController.extend({
-            layoutTemplate: 'boafm',
-            yieldTemplates: {
-                'header': {to: 'headerRadioBOAFM'},
-            }
-          })
-        }
-    },
-
     functionsGeneric: {
         validateUserLogin: function(){
             if(Meteor.userId === undefined){
@@ -445,46 +401,6 @@ Meteor.zion = {
                 defaultValue: (new Date()).getDate() + '/' + ((new Date()).getMonth()+1) + '/' + (new Date()).getFullYear() + ' ' + (new Date()).getHours() + ':' +  (new Date()).getMinutes() + ':' + (new Date()).getSeconds()
             }
         }),
-    },
-
-    routers: function(){
-        Router.configure({
-            layoutTemplate: 'ApplicationLayout'
-        });
-
-        Router.route('/', {
-            controller: Meteor.zion.home.controller
-        });
-
-        Router.route('/login', {
-            name: 'login',
-            controller: Meteor.zion.login.controller
-        });
-
-        Router.route('/television', {
-            name: 'television',
-            controller: Meteor.zion.television.controller
-        });
-
-        Router.route('/newspaper', {
-            name: 'newspaper',
-            controller: Meteor.zion.newspaper.controller
-        });
-
-        Router.route('/portal', {
-            name: 'portal',
-            controller: Meteor.zion.portal.controller
-        });
-
-        /*Router.route('/boafm', {
-            name: 'boafm',
-            controller: Meteor.zion.boafm.radio.controller
-        });
-
-        Router.route('/fmmn', {
-            name: 'fmmn',
-            controller: Meteor.zion.boafm.radio.controller
-        });*/
     }
 };
 
@@ -495,6 +411,5 @@ Meteor.zion.collections.notification.attachSchema(Meteor.zion.schemas.notificati
 Meteor.zion.collections.subscription.attachSchema(Meteor.zion.schemas.subscription);
 Meteor.zion.collections.lastcomment.attachSchema(Meteor.zion.schemas.lastcomment);
 Meteor.zion.collections.userban.attachSchema(Meteor.zion.schemas.userban);
-Meteor.zion.routers();
 
 //Meteor.zion.collections.instituition.insert({name: "Meio Norte"}, function(error, result) {});
