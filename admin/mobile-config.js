2cd27a36f3cf8b14558bb5678b2897db2c226d94
Alisson <alissonplus@msn.com>
2015-08-25 20:35:31 -0300

    mobile config
    - adicionado o arquivo mobile_config
    - adicionado os splash

---
+++ admin/mobile-config.js
@@ -0,0 +1,47 @@
+App.info({
+  name: 'Meio Norte',
+  description: 'Aplicativo da Meio Norte',
+  version: '0.0.1'
+});
+
+App.icons({
+
+  // iOS
+  'iphone': 'public/images/icons/icon-60x60.png',
+  'iphone_2x': 'public/images/icons/icon-60x60@2x.png',
+  'ipad': 'public/images/icons/icon-72x72.png',
+  'ipad_2x': 'public/images/icons/icon-72x72@2x.png',
+
+  // Android
+  'android_ldpi': 'public/images/icons/icon_ldpi.png',
+  'android_mdpi': 'public/images/icons/icon_mdpi.png',
+  'android_hdpi': 'public/images/icons/icon_hdpi.png',
+  'android_xhdpi': 'public/images/icons/icon_xhdpi.png'
+
+});
+
+App.launchScreens({
+
+  // iOS
+  'iphone': 'public/images/splash/splash-320x480.png',
+  'iphone_2x': 'public/images/splash/splash-320x480@2x.png',
+  'iphone5': 'public/images/splash/splash-320x568@2x.png',
+  'ipad_portrait': 'public/images/splash/splash-768x1024.png',
+  'ipad_portrait_2x': 'public/images/splash/splash-768x1024@2x.png',
+  'ipad_landscape': 'public/images/splash/splash-1024x768.png',
+  'ipad_landscape_2x': 'public/images/splash/splash-1024x768@2x.png',
+
+  // Android
+  'android_ldpi_portrait': 'public/images/splash/splash-200x320.png',
+  'android_ldpi_landscape': 'public/images/splash/splash-320x200.png',
+  'android_mdpi_portrait': 'public/images/splash/splash-320x480.png',
+  'android_mdpi_landscape': 'public/images/splash/splash-480x320.png',
+  'android_hdpi_portrait': 'public/images/splash/splash-480x800.png',
+  'android_hdpi_landscape': 'public/images/splash/splash-800x480.png',
+  'android_xhdpi_portrait': 'public/images/splash/splash-720x1280.png',
+  'android_xhdpi_landscape': 'public/images/splash/splash-1280x720.png'
+});
+
+//App.accessRule("*://zapzap.club/*");
+//App.accessRule("*://redemn-admin.meteor.com/*");
+//App.accessRule("*://localhost:3010/*");
