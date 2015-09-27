ca15eeb1ae41047248f986d1b5c622b3aa08a993
Allan Alexandre <alexandesigner@gmail.com>
2015-09-27 15:38:44 -0300

    Mensage text input

--- app/client/templates/pages/television/pages/program/components/message/addMessage.js
+++ app/client/templates/pages/television/pages/program/components/message/addMessage.js
@@ -1,5 +1,9 @@
 Template.addMessage.events({
 
+  'submit form#addMessage' : function(){
+    console.log(opa entrou);
+  },
+
   'focus #btn-show-add-image, click #btn-show-add-image': function () {
     document.querySelector('body').classList.add('add-image-buttons-television');
   },
