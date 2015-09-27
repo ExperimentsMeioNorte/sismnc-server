b6efa393730194225263e639bf4700d0d14643ae
Alisson <alissonplus@msn.com>
2015-09-27 15:46:39 -0300

    collection
    - adicionado a nova collection content

--- app/lib/collections.js
+++ app/lib/collections.js
@@ -1,21 +1,24 @@
 //conexão
-Meteor.remote = DDP.connect("http://zapzap.club:3000"); // em producao
-//Meteor.remote = DDP.connect("http://localhost:3000"); // em desenvolvimento
+//Meteor.remote = DDP.connect("http://zapzap.club:3000"); // em producao
+Meteor.remote = DDP.connect("http://localhost:3000"); // em desenvolvimento
 
 //collection
 User = new Meteor.Collection('user', Meteor.remote);
 Program = new Meteor.Collection('program', Meteor.remote);
 Category = new Meteor.Collection('category', Meteor.remote);
+Content = new Meteor.Collection('content', Meteor.remote);
 
 //configurações automatizadas
 collectionsName = {
     User: 'user',
     Program: 'program',
-    Category: 'category'
+    Category: 'category',
+    Content: 'content'
 };
 
 collections = {
     User: User,
     Program: Program,
-    Category: Category
+    Category: Category,
+    Content: Content
 };
