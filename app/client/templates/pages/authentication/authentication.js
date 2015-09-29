861c5a07360131146937e186341372bcc311c703
Allan Alexandre <alexandesigner@gmail.com>
2015-09-28 22:23:44 -0300

    Add Google Account

--- app/client/templates/pages/authentication/authentication.js
+++ app/client/templates/pages/authentication/authentication.js
@@ -91,5 +91,66 @@ Template.authentication.events({
           }
         }
       });
+    },
+    'click .bg-google': function (evento, tmp) {
+      evento.preventDefault();
+
+      // acessa o methodo das configuracoes para efetuar o login de uma determinada rede social
+      Meteor.loginApp(evento);
+
+      // atributos montados a partir do methodo loginApp, como as opcoes e qual servidor de login é para executar
+      Meteor.loginAppService(Meteor.loginAppOptions, function(err){
+        if (err){
+          console.log('Não deu certo, tenta novamente mais tarde');
+        }else{
+          var usersSearch = Meteor.users.findOne({_id:Meteor.userId()});
+          var userId = User.findOne(
+              {
+                google_id:usersSearch.services.google.id,
+                email:usersSearch.services.google.email
+              }
+          );
+
+          if(userId !== undefined){
+            if(userId.status === 0){
+              console.log('Sem autorizacão');
+            }else{
+              Meteor.remote.setUserId(userId._id);
+              Router.go('home');
+            }
+          }else{
+            Meteor.remote.call(
+                'insertUser',
+                [
+                  111,
+                  '0',
+                  usersSearch.services.google.name,
+                  usersSearch.services.google.picture,
+                  usersSearch.services.google.email,
+                  null,
+                  usersSearch.services.google.id,
+                  null,
+                  2
+                ],
+                function(error, result){
+                  if(error){
+                    console.log('Algo deu errado');
+                  }else{
+                    console.log('Ae deu certo');
+                    var userId = User.findOne(
+                      {
+                        google_id:usersSearch.services.google.id,
+                        email:usersSearch.services.google.email
+                      }
+                    );
+
+                    Meteor.remote.setUserId(userId._id);
+                    Router.go('home');
+                  }
+                }
+            );
+          }
+        }
+      });
     }
 });
