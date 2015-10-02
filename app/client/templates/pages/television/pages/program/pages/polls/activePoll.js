174e5afe2fbe169a75a4915f715eadc847168b6e
Alisson <alissonplus@msn.com>
2015-10-02 20:35:32 -0300

    Poll
    - implementacao final

--- app/client/templates/pages/television/pages/program/pages/polls/activePoll.js
+++ app/client/templates/pages/television/pages/program/pages/polls/activePoll.js
@@ -1,3 +1,87 @@
+Template.activePoll.helpers({
+    // mostra a enquete
+    pollActive: function(){
+        var poll = Poll.find(
+          {
+            status: 1,
+            program_id: Router.current().params._id
+          }
+        ).map(
+          function(p){
+            return {
+              _id: p._id,
+              description: p.description,
+              img: p.img
+            }
+          }
+        );
+
+        // validate poll
+        if(poll && poll[0] !== undefined){
+          var pollUser = PollUser.find(
+          {
+              status: 1,
+              poll_id: poll[0]._id,
+              user_id: Meteor.remote.userId()
+          }).map(
+            function(pu){
+              return {
+                _id: pu._id
+              }
+            }
+          );
+
+          if((pollUser && pollUser[0] !== undefined)
+            && (document.querySelector('.polls-answers') !== null
+            && document.querySelector('.results-question') !== null)){
+            document.querySelector('.polls-answers').classList.add('hide');
+            document.querySelector('.results-question').classList.remove('hide');
+          }
+        }else if(document.querySelector('.polls-answers') !== null
+            && document.querySelector('.message-feedback') !== null){
+            document.querySelector('.polls-answers').classList.add('hide');
+            document.querySelector('.message-feedback').classList.remove('hide');
+        }
+
+        return poll;
+    },
+  // mostra as respostas
+  answersActive: function(){
+    var poll = Poll.find(
+      {
+        status: 1,
+        program_id: Router.current().params._id
+      }
+    ).map(
+      function(p){
+        return {
+          _id: p._id,
+          description: p.description,
+          img: p.img
+        }
+      }
+    );
+
+    if(poll && poll[0] !== undefined){
+      return Answer.find(
+        {
+          status: 1,
+          poll_id: poll[0]._id
+        }
+      ).map(
+        function(a){
+          return {
+            _id: a._id,
+            description: a.description
+          }
+        }
+      );
+    }else{
+      return '';
+    }
+  }
+});
+
 Template.activePoll.events({
     'click .btn-answer': function(event){
         event.preventDefault();
